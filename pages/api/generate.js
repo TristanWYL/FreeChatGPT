89177359c4fd5e1af62649cd5ca4d1a2f9af7ad8
Tristan <tristan_wyl@hotmail.com>
2023-02-08 23:31:24 +0800

    Add error handling for vercel timeout request

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -41,10 +41,8 @@ export default async function (req, res) {
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
-      console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
     } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
           message: "An error occurred during your request.",
